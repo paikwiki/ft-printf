b293a134709c31ff98e34f71163a9158d3d1d6c1
cbaek <cbaek@student.42seoul.kr>
2020-03-27 23:20:09 +0900

    Edit ft_printf(): va_arg()

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/13 22:23:23 by cbaek             #+#    #+#             */
-/*   Updated: 2020/03/27 21:34:46 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/03/27 22:36:25 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,7 @@ void	ft_printf(const char *str, ...)
 {
 	va_list	ap;
 	int		arg_len;
+	int		arg_int;
 
 	// 1. 연결리스트인 `ph_list`와 `lst_args`를 만든다.
 	t_list	*ph_list;
@@ -42,10 +43,12 @@ void	ft_printf(const char *str, ...)
 
 	arg_len = ft_lstsize(ph_list);
 	// 3. `lst_args`에 가변인자를 담는다.
-	va_start(ap, str);
+	va_start(ap, arg_len);
+	arg_int = 0;
 	while (arg_len > 0)
 	{
-		va_arg(ap, int);
+		arg_int = va_arg(ap, int);
+		arg_len--;
 	}
 	va_end(ap);
 	// 4. `ph_list`의 길이와 `lst_args`의 길이를 비교한다. `ph_list`의 길이가 더 길면 오류 반환
@@ -54,6 +57,4 @@ void	ft_printf(const char *str, ...)
 	// 7. 자리지정자로 판단되면 `ph_list`와 `lst_args`로부터 하나씩 꺼내와서 데이터 처리/출력한다.
 	// 8. 5~7번 과정을 문자열 끝까지 진행한다.
 	free(ph_list);
-
-
 }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/13 15:56:16 by cbaek             #+#    #+#             */
-/*   Updated: 2020/03/27 20:58:43 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/03/27 22:05:55 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,8 @@ int main(void)
 {
 	int len;
 
-	char *str = "idx: %d, str: %s, number: %.3f, percent char: %%\n";
-	// printf(str, 123, "test", 2.345);
-	ft_printf(str, 123, "test", 2.345);
+	// char *str = "idx: %d, str: %s, number: %.3f, percent char: %%\n";
+	char *str = "first: %dst, second: %dnd, third: %drd\n";
+	ft_printf(str, 1, 2, 3);
 	return (0);
 }
