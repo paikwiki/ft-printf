24029ca0e4f8b62fcb899ae6ec2846cd1a3d6fe7
cbaek <cbaek@student.42seoul.kr>
2020-08-04 22:08:54 +0900

    Refactor: create proc_ft_printf()

--- codes/ft_printf.c
+++ codes/ft_printf.c
@@ -6,27 +6,25 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/13 22:23:23 by cbaek             #+#    #+#             */
-/*   Updated: 2020/08/04 21:42:50 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/08/04 22:07:38 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"ft_printf.h"
 
-int			ft_printf(const char *format, ...)
+static int	proc_ft_printf(const char *format, va_list ap)
 {
-	va_list	ap;
 	char	*str;
-	int		current_idx;
+	int		idx;
 	int		total_len;
 
-	va_start(ap, format);
-	current_idx = 0;
+	idx = 0;
 	total_len = 0;
-	while (format[current_idx] != '\0')
+	while (format[idx] != '\0')
 	{
-		if (format[current_idx] != '%')
+		if (format[idx] != '%')
 		{
-			ft_putchar_fd(format[current_idx], 1);
+			ft_putchar_fd(format[idx], 1);
 			total_len = total_len + 1;
 		}
 		else
@@ -35,9 +33,18 @@ int			ft_printf(const char *format, ...)
 			ft_putstr_fd(str, 1);
 			total_len = total_len + ft_strlen(str);
 		}
-		++current_idx;
+		++idx;
 	}
-	va_end(ap);
-
 	return (total_len);
+}
+
+int			ft_printf(const char *format, ...)
+{
+	va_list	ap;
+	int		len;
+
+	va_start(ap, format);
+	len = proc_ft_printf(format, ap);
+	va_end(ap);
+	return (len);
 }
