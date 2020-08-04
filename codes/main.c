a3a3aaab80fb6c3521a008e9c314caac982a573d
cbaek <cbaek@student.42seoul.kr>
2020-08-05 01:34:23 +0900

    Edit tests and Create a test for "d"

--- codes/main.c
+++ codes/main.c
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/13 15:56:16 by cbaek             #+#    #+#             */
-/*   Updated: 2020/08/05 00:54:27 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/08/05 01:33:24 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,33 +15,57 @@
 
 int main(void)
 {
-	int	len;
+	int	len01;
+	int len02;
 
-	len = 0;
+	len01 = 0;
+	len02 = 0;
 
 	/* test 01 */
-	len = ft_printf("ft_printf : Doby is Free!");
-	ft_printf("\nlen: ");
-	ft_putnbr_fd(len, 1);
+	len01 = ft_printf("ft_printf : Doby is Free!");
 	ft_printf("\n");
-	len = printf("printf    : Doby is Free!");
-	printf("\nlen: %d\n", len);
+	len02 = ft_printf("ft_printf : Cbaek is in a jail!");
+	ft_printf("\nlen01: ");
+	ft_putnbr_fd(len01, 1);
+	ft_printf(" | len02: ");
+	ft_putnbr_fd(len02, 1);
+	ft_printf("\n");
+	len01 = printf("   printf : Doby is Free!");
+	printf("\n");
+	len02 = printf("   printf : Cbaek is in a jail!");
+	printf("\nlen01: %d", len01);
+	printf(" | len02: %d\n", len02);
 
 	/* test 02 */
-	len = ft_printf("ft_printf : %s is %s", "Doby", "free!");
-	ft_printf("\nlen: ");
-	ft_putnbr_fd(len, 1);
+	len01 = ft_printf("ft_printf : %s is %s", "Doby", "free!");
 	ft_printf("\n");
-	len = printf("printf    : %s is %s", "Doby", "free!");
-	printf("\nlen: %d\n", len);
+	len02 = ft_printf("ft_printf : %s is %s", "Cbaek", "in a jail!");
+	ft_printf("\nlen01: ");
+	ft_putnbr_fd(len01, 1);
+	ft_printf(" | len02: ");
+	ft_putnbr_fd(len02, 1);
+	ft_printf("\n");
+	len01 = printf("   printf : %s is %s", "Doby", "free!");
+	printf("\n");
+	len02 = printf("   printf : %s is %s", "Cbaek", "in a jail!");
+	printf("\nlen01: %d", len01);
+	printf(" | len02: %d\n", len02);
 
 	/* test 03 */
-	len = ft_printf("ft_printf : %s is %s", "Cbaek", "in a jail!");
-	ft_printf("\nlen: ");
-	ft_putnbr_fd(len, 1);
+	len01 = ft_printf("ft_printf : %s is %d", "Doby", 100);
 	ft_printf("\n");
-	len = printf("printf    : %s is %s", "Cbaek", "in a jail!");
-	printf("\nlen: %d\n", len);
+	len02 = ft_printf("ft_printf : %s is %d", "Cbaek", 98);
+	ft_printf("\nlen01: ");
+	ft_putnbr_fd(len01, 1);
+	ft_printf(" | len02: ");
+	ft_putnbr_fd(len02, 1);
+	ft_printf("\n");
+	len01 = printf("   printf : %s is %d", "Doby", 100);
+	printf("\n");
+	len02 = printf("   printf : %s is %d", "Cbaek", 98);
+	printf("\nlen01: %d", len01);
+	printf(" | len02: %d\n", len02);
+
 
 	return (0);
 }
