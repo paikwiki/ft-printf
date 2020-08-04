9af0e196276ae3564c640bbb122ad1f6dfcf8953
cbaek <cbaek@student.42seoul.kr>
2020-08-04 22:00:51 +0900

    Return integer value after execute ft_printf()
    - 현재 위치(current_idx)와 전체 길이(total_len)를 위한 Int 변수 선언

--- codes/ft_printf.c
+++ codes/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/13 22:23:23 by cbaek             #+#    #+#             */
-/*   Updated: 2020/08/04 20:59:09 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/08/04 21:42:50 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,28 @@ int			ft_printf(const char *format, ...)
 {
 	va_list	ap;
 	char	*str;
+	int		current_idx;
+	int		total_len;
 
 	va_start(ap, format);
-	while (*format)
+	current_idx = 0;
+	total_len = 0;
+	while (format[current_idx] != '\0')
 	{
-		if (*format != '%')
-			ft_putchar_fd(*format, 1);
+		if (format[current_idx] != '%')
+		{
+			ft_putchar_fd(format[current_idx], 1);
+			total_len = total_len + 1;
+		}
 		else
 		{
 			str = va_arg(ap, char *);
 			ft_putstr_fd(str, 1);
+			total_len = total_len + ft_strlen(str);
 		}
-		++format;
+		++current_idx;
 	}
 	va_end(ap);
 
-	return (0);
+	return (total_len);
 }
