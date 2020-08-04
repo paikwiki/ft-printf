2ba8a56a0c62624823db65a02d764ff83aa15fbc
cbaek <cbaek@student.42seoul.kr>
2020-08-05 01:13:46 +0900

    Define TYPES(scpdiuxX%)

--- codes/ft_printf.h
+++ codes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/13 15:43:46 by cbaek             #+#    #+#             */
-/*   Updated: 2020/08/04 21:03:21 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/08/05 01:05:48 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,6 @@
 # include "libft/libft.h"
 # include <stdarg.h>
 
+# define TYPES "cspdiuxX%"
 int	ft_printf(const char *format, ...);
 #endif
