d9a7315e4ea6c107e27f2161197708aba62cac72
cbaek <cbaek@student.42seoul.kr>
2020-08-05 00:59:16 +0900

    Add test: print plain text without variable args

--- codes/main.c
+++ codes/main.c
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/13 15:56:16 by cbaek             #+#    #+#             */
-/*   Updated: 2020/08/04 22:22:30 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/08/05 00:54:27 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,14 @@ int main(void)
 	len = 0;
 
 	/* test 01 */
+	len = ft_printf("ft_printf : Doby is Free!");
+	ft_printf("\nlen: ");
+	ft_putnbr_fd(len, 1);
+	ft_printf("\n");
+	len = printf("printf    : Doby is Free!");
+	printf("\nlen: %d\n", len);
+
+	/* test 02 */
 	len = ft_printf("ft_printf : %s is %s", "Doby", "free!");
 	ft_printf("\nlen: ");
 	ft_putnbr_fd(len, 1);
@@ -27,7 +35,7 @@ int main(void)
 	len = printf("printf    : %s is %s", "Doby", "free!");
 	printf("\nlen: %d\n", len);
 
-	/* test 02 */
+	/* test 03 */
 	len = ft_printf("ft_printf : %s is %s", "Cbaek", "in a jail!");
 	ft_printf("\nlen: ");
 	ft_putnbr_fd(len, 1);
