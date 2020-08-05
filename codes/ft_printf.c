7e91c8ed102bfad1c6858b020586358f51df0c7d
cbaek <cbaek@student.42seoul.kr>
2020-08-05 15:55:12 +0900

    Lint code and add free(str)

--- codes/ft_printf.c
+++ codes/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/13 22:23:23 by cbaek             #+#    #+#             */
-/*   Updated: 2020/08/05 15:43:45 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/08/05 15:54:43 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,17 @@ static const char	*get_placeholder(const char *format, int *idx)
 	int		ph_idx;
 
 	len = 1;
-	while (ft_strchr(TYPES, format[*idx + len++]) == 0) /* 형식 필드를 만날때까지 while 실행 */
-		;
+	while (ft_strchr(TYPES, format[*idx + len]) == 0)
+		++len;
 	placeholder = (char *)malloc(sizeof(char) * (len + 1));
 	placeholder[len] = '\0';
 	ph_idx = 0;
-	while (len > ph_idx)
+	while (len >= ph_idx)
 	{
 		placeholder[ph_idx] = format[*idx + ph_idx];
 		++ph_idx;
 	}
-	*idx = *idx + (len - 1);
+	*idx = *idx + len;
 	return ((const char *)placeholder);
 }
 
@@ -49,12 +49,12 @@ static char			*get_typed_arg(const char *placeholder, va_list ap)
 	}
 	else if (ph_type == 's')
 		str = ft_strdup(va_arg(ap, char *));
-	// else if (ph_type == 'p')
-	// 	;
 	else if (ph_type == 'd')
 		str = ft_strdup(ft_itoa(va_arg(ap, int)));
 	else if (ph_type == 'i')
 		str = ft_strdup(ft_itoa(va_arg(ap, int)));
+	// else if (ph_type == 'p')
+	// 	;
 	// else if (ph_type == 'u')
 	// 	;
 	// else if (ph_type == 'x')
@@ -93,11 +93,13 @@ static int			proc_ft_printf(const char *format, va_list ap)
 		{
 			placeholder = get_placeholder(format, &idx);
 			str = get_typed_arg(placeholder, ap);
+			/* TODO: 가져온 str에 형식 필드 외의 다른 필드 적용 */
 			ft_putstr_fd(str, 1);
 			total_len = total_len + ft_strlen(str);
 		}
 		++idx;
 	}
+	free(str);
 	return (total_len);
 }
 
