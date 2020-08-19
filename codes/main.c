ccb5597946e9d1c71134f306305df28acb88f2e7
cbaek <cbaek@student.42seoul.kr>
2020-08-19 19:43:29 +0900

    Change text for the test

--- codes/main.c
+++ codes/main.c
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/13 15:56:16 by cbaek             #+#    #+#             */
-/*   Updated: 2020/08/19 19:38:21 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/08/19 19:42:05 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,48 +27,48 @@ int main(void)
 	printf("================================= \n\n");
 	/* test 01 - plain text */
 	printf("====== 01 - plain text ========== \n");
-	len01 = ft_printf("ft_printf : Doby is Free!");
+	len01 = ft_printf("ft_printf : Andy is Free!");
 	ft_printf("\n");
-	len02 = ft_printf("ft_printf : Cbaek is in a jail!");
+	len02 = ft_printf("ft_printf : Red is in a jail!");
 	ft_printf("\nlen01: %d | len02: %d\n", len01, len02);
 
-	len01 = printf("   printf : Doby is Free!");
+	len01 = printf("   printf : Andy is Free!");
 	printf("\n");
-	len02 = printf("   printf : Cbaek is in a jail!");
+	len02 = printf("   printf : Red is in a jail!");
 	printf("\nlen01: %d | len02: %d\n", len01, len02);
 
 	/* test 02 - s */
 	printf("====== test 02 - s ========== \n");
-	len01 = ft_printf("ft_printf : %s is %s", "Doby", "free!");
+	len01 = ft_printf("ft_printf : %s is %s", "Andy", "free!");
 	ft_printf("\n");
-	len02 = ft_printf("ft_printf : %s is %s", "Cbaek", "in a jail!");
+	len02 = ft_printf("ft_printf : %s is %s", "Red", "in a jail!");
 	ft_printf("\nlen01: %d | len02: %d\n", len01, len02);
 
-	len01 = printf("   printf : %s is %s", "Doby", "free!");
+	len01 = printf("   printf : %s is %s", "Andy", "free!");
 	printf("\n");
-	len02 = printf("   printf : %s is %s", "Cbaek", "in a jail!");
+	len02 = printf("   printf : %s is %s", "Red", "in a jail!");
 	printf("\nlen01: %d | len02: %d\n", len01, len02);
 
 	/* test 03 - d */
 	printf("====== test 03 - d ========== \n");
-	len01 = ft_printf("ft_printf : %s is %d", "Doby", 100);
+	len01 = ft_printf("ft_printf : %s is %d", "Andy", 100);
 	ft_printf("\n");
-	len02 = ft_printf("ft_printf : %s is %d", "Cbaek", 98);
+	len02 = ft_printf("ft_printf : %s is %d", "Red", 98);
 	ft_printf("\nlen01: %d | len02: %d\n", len01, len02);
 
-	len01 = printf("   printf : %s is %d", "Doby", 100);
+	len01 = printf("   printf : %s is %d", "Andy", 100);
 	printf("\n");
-	len02 = printf("   printf : %s is %d", "Cbaek", 98);
+	len02 = printf("   printf : %s is %d", "Red", 98);
 	printf("\nlen01: %d | len02: %d\n", len01, len02);
 
-	len01 = ft_printf("ft_printf : %s is %d", "Doby", INT_MAX);
+	len01 = ft_printf("ft_printf : %s is %d", "Andy", INT_MAX);
 	ft_printf("\n");
-	len02 = ft_printf("ft_printf : %s is %d", "Cbaek", INT_MIN);
+	len02 = ft_printf("ft_printf : %s is %d", "Red", INT_MIN);
 	ft_printf("\nlen01: %d | len02: %d\n", len01, len02);
 
-	len01 = printf("   printf : %s is %d", "Doby", INT_MAX);
+	len01 = printf("   printf : %s is %d", "Andy", INT_MAX);
 	printf("\n");
-	len02 = printf("   printf : %s is %d", "Cbaek", INT_MIN);
+	len02 = printf("   printf : %s is %d", "Red", INT_MIN);
 	printf("\nlen01: %d | len02: %d\n", len01, len02);
 
 	/* test 04 - c */
