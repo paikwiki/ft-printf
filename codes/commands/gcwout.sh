7916f5cac71b4654453ca5f184571deafd0bc79d
cbaek <cbaek@student.42seoul.kr>
2020-08-20 12:05:46 +0900

    Edit command for the vacode task
    - Compile *.c in root folder

--- codes/commands/gcwout.sh
+++ codes/commands/gcwout.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # proot is project working directory. It is defined .vscode/tasks.json
-gcc -Wall -Wextra -Werror ${proot}/main.c ${proot}/ft_*.c \
+gcc -Wall -Wextra -Werror ${proot}/*.c \
 	${proot}/libft/*.c -o ${proot}/a.out && ${proot}/a.out
