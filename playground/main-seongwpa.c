41ec80f05d168daa452fa9501354891c69624300
cbaek <cbaek@student.42seoul.kr>
2020-08-21 12:23:22 +0900

    Create main-seongwpa.c for the test

---
+++ playground/main-seongwpa.c
@@ -0,0 +1,110 @@
+#include "ft_printf.h"
+#include <stdio.h>
+/*
+//s
+int main()
+{
+	ft_printf("%*.*s\n",10 ,6 ,"abcde");
+	printf("%*.*s\n",10 ,6 ,"abcde");
+	ft_printf("%*.*s\n",6 ,8 ,"abcde");
+	printf("%*.*s\n",6 ,8 ,"abcde");
+	ft_printf("%*.*s\n",10 ,4 ,"abcde");
+	printf("%*.*s\n",10 ,4 ,"abcde");
+	ft_printf("%*.*s\n",4 ,6 ,"abcde");
+	printf("%*.*s\n",4 ,6 ,"abcde");
+	ft_printf("%*.*s\n",4 ,3 ,"abcde");
+	printf("%*.*s\n",4 ,3 ,"abcde");
+	ft_printf("%*.*s\n",3 ,4 ,"abcde");
+	printf("%*.*s\n",3 ,4 ,"abcde");
+}
+*/
+/*
+//d
+int main()
+{
+	ft_printf("%*.*d\n",10 ,6 ,23452);
+	printf("%*.*d\n",10 ,6 ,23452);
+	ft_printf("%*.*d\n",6 ,8 ,23452);
+	printf("%*.*d\n",6 ,8 ,23452);
+	ft_printf("%*.*d\n",10 ,4 ,23452);
+	printf("%*.*d\n",10 ,4 ,23452);
+	ft_printf("%*.*d\n",4 ,6 ,23452);
+	printf("%*.*d\n",4 ,6 ,23452);
+	ft_printf("%*.*d\n",4 ,3 ,23452);
+	printf("%*.*d\n",4 ,3 ,23452);
+	ft_printf("%*.*d\n",3 ,4 ,23452);
+	printf("%*.*d\n",3 ,4 ,23452);
+}
+*/
+/*
+//i
+int main()
+{
+	ft_printf("%*.*i\n",10 ,6 ,23452);
+	printf("%*.*i\n",10 ,6 ,23452);
+	ft_printf("%*.*i\n",6 ,8 ,23452);
+	printf("%*.*i\n",6 ,8 ,23452);
+	ft_printf("%*.*i\n",10 ,4 ,23452);
+	printf("%*.*i\n",10 ,4 ,23452);
+	ft_printf("%*.*i\n",4 ,6 ,23452);
+	printf("%*.*i\n",4 ,6 ,23452);
+	ft_printf("%*.*i\n",4 ,3 ,23452);
+	printf("%*.*i\n",4 ,3 ,23452);
+	ft_printf("%*.*i\n",3 ,4 ,23452);
+	printf("%*.*i\n",3 ,4 ,23452);
+}
+*/
+/*
+//u
+int main()
+{
+	ft_printf("%*.*u\n",10 ,6 ,23452);
+	printf("%*.*u\n",10 ,6 ,23452);
+	ft_printf("%*.*u\n",6 ,8 ,23452);
+	printf("%*.*u\n",6 ,8 ,23452);
+	ft_printf("%*.*u\n",10 ,4 ,23452);
+	printf("%*.*u\n",10 ,4 ,23452);
+	ft_printf("%*.*u\n",4 ,6 ,23452);
+	printf("%*.*u\n",4 ,6 ,23452);
+	ft_printf("%*.*u\n",4 ,3 ,23452);
+	printf("%*.*u\n",4 ,3 ,23452);
+	ft_printf("%*.*u\n",3 ,4 ,23452);
+	printf("%*.*u\n",3 ,4 ,23452);
+}
+*/
+/*
+//x
+int main()
+{
+	ft_printf("%*.*x\n",10 ,6 ,23452);
+	printf("%*.*x\n",10 ,6 ,23452);
+	ft_printf("%*.*x\n",6 ,8 ,23452);
+	printf("%*.*x\n",6 ,8 ,23452);
+	ft_printf("%*.*x\n",10 ,4 ,23452);
+	printf("%*.*x\n",10 ,4 ,23452);
+	ft_printf("%*.*x\n",4 ,6 ,23452);
+	printf("%*.*x\n",4 ,6 ,23452);
+	ft_printf("%*.*x\n",4 ,3 ,23452);
+	printf("%*.*x\n",4 ,3 ,23452);
+	ft_printf("%*.*x\n",3 ,4 ,23452);
+	printf("%*.*x\n",3 ,4 ,23452);
+}
+*/
+/*
+//X
+int main()
+{
+	ft_printf("%*.*X\n",10 ,6 ,23452);
+	printf("%*.*X\n",10 ,6 ,23452);
+	ft_printf("%*.*X\n",6 ,8 ,23452);
+	printf("%*.*X\n",6 ,8 ,23452);
+	ft_printf("%*.*X\n",10 ,4 ,23452);
+	printf("%*.*X\n",10 ,4 ,23452);
+	ft_printf("%*.*X\n",4 ,6 ,23452);
+	printf("%*.*X\n",4 ,6 ,23452);
+	ft_printf("%*.*X\n",4 ,3 ,23452);
+	printf("%*.*X\n",4 ,3 ,23452);
+	ft_printf("%*.*X\n",3 ,4 ,23452);
+	printf("%*.*X\n",3 ,4 ,23452);
+}
+*/
