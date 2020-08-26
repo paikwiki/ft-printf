7f7a255f190f4e8a24ba4762f2a270136f436b9d
cbaek <cbaek@student.42seoul.kr>
2020-08-26 12:58:34 +0900

    Refactor: % character

--- codes/put_percent_type.c
+++ codes/put_percent_type.c
@@ -6,48 +6,45 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/20 11:51:33 by cbaek             #+#    #+#             */
-/*   Updated: 2020/08/26 11:16:03 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/08/26 12:56:47 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static t_putcounts	calc(size_t arg, size_t w)
+static void	calc(int arg, t_struct *note)
 {
-	t_putcounts	pcnt;
-
-	pcnt.arg = arg;
-	pcnt.zero = 0;
-	if (w > 0 && w > arg)
-		pcnt.space = w - arg;
+	note->arg = arg;
+	note->zero = 0;
+	if (note->width > 0 && note->width > arg)
+		note->space = note->width - arg;
 	else
-		pcnt.space = 0;
-	return (pcnt);
+		note->space = 0;
+	return ;
 }
 
-size_t 				put_percent_type(t_struct *note)
+size_t 		put_percent_type(t_struct *note)
 {
-	size_t		proc_len;
-	t_putcounts	pcnt;
+	int	proc_len;
 
 	proc_len = 1;
-	pcnt = calc(proc_len, note->width);
+	calc(proc_len, note);
 	if (note->flag == '-')
 	{
-		write(1, "%", pcnt.arg);
-		proc_len += putnchar('0', pcnt.zero);
-		proc_len += putnchar(' ', pcnt.space);
+		write(1, "%", note->arg);
+		proc_len += putnchar('0', note->zero);
+		proc_len += putnchar(' ', note->space);
 	}
 	else if (note->flag == '0')
 	{
-		proc_len += putnchar('0', pcnt.space);
-		write(1, "%", pcnt.arg);
+		proc_len += putnchar('0', note->space);
+		write(1, "%", note->arg);
 	}
 	else
 	{
-		proc_len += putnchar(' ', pcnt.space);
-		proc_len += putnchar('0', pcnt.zero);
-		write(1, "%", pcnt.arg);
+		proc_len += putnchar(' ', note->space);
+		proc_len += putnchar('0', note->zero);
+		write(1, "%", note->arg);
 	}
 	return (proc_len);
 }
