ae6ef224150b558afa0dc7c0e50cd60f87ac9d79
cbaek <cbaek@student.42seoul.kr>
2020-08-26 12:51:11 +0900

    Refactor: s field

--- codes/put_s_type.c
+++ codes/put_s_type.c
@@ -6,63 +6,63 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/20 12:09:45 by cbaek             #+#    #+#             */
-/*   Updated: 2020/08/26 11:16:03 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/08/26 12:50:07 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include <stdio.h>
-static t_putcounts	calc(size_t arg, size_t width, size_t prcs, int is_dot)
+static void	calc(int arg, t_struct *note)
 {
-	t_putcounts	pcnt;
-
-	pcnt.zero = 0;
-	if (arg >= width) // 456
+	note->zero = 0;
+	if (arg >= note->width) // 456
 	{
-		if (is_dot == 1 && arg >= prcs) // 56
+		if (note->is_dot == 1 && arg >= note->prcs) // 56
 		{
-			pcnt.arg = prcs;
-			pcnt.space = width >= prcs ? width - prcs : 0; // 5, 6
+			note->arg = note->prcs < 0 ? arg : note->prcs;
+			note->space = note->width >= note->prcs && note->prcs >= 0 ? note->width - note->prcs : 0; // 5, 6
 		}
 		else // 4
 		{
-			pcnt.arg = arg;
-			pcnt.space = 0;
+			note->arg = arg;
+			note->space = 0;
 		}
 	}
 	else // 123
 	{
-		pcnt.arg = (arg >= prcs && prcs != 0) ? prcs : arg;
-		pcnt.space = (arg >= prcs && is_dot != 0) ? width - prcs : width - arg;
+		note->arg = (arg >= note->prcs) && note->prcs > 0 ? note->prcs : arg;
+		if (note->prcs < 0)
+			note->space = note->width - note->arg;
+		else
+			note->space = (arg >= note->prcs) && note->is_dot != 0 ? note->width - note->prcs : note->width - arg;
 	}
-	return (pcnt);
+	return ;
 }
 
-size_t	put_s_type(char *arg, t_struct *note)
+size_t		put_s_type(char *arg, t_struct *note)
 {
-	size_t		proc_len;
-	char		*str;
-	t_putcounts	pcnt;
+	int		proc_len;
+	char	*str;
 
 	if (arg == NULL)
 		str = ft_strdup(PRINT_NULL);
 	else
 		str = ft_strdup(arg);
 	proc_len = ft_strlen(str);
-	pcnt = calc(proc_len, note->width, note->prcs, note->is_dot);
-	pcnt.arg = (note->is_dot == 1 && note->prcs == 0) ? 0 : pcnt.arg;
-	proc_len = pcnt.arg;
+	calc(proc_len, note);
+	note->arg = (note->is_dot == 1 && note->prcs == 0) ? 0 : note->arg;
+	proc_len = note->arg;
 	if (note->is_dot == 1 && note->width == 0 && note->prcs == 0)
 		return (0);
 	if (note->flag == '-') {
-		write(1, str, pcnt.arg);
-		proc_len += putnchar('0', pcnt.zero);
-		proc_len += putnchar(' ', pcnt.space);
+		write(1, str, note->arg);
+		proc_len += putnchar('0', note->zero);
+		proc_len += putnchar(' ', note->space);
 	}
 	else {
-		proc_len += putnchar(' ', pcnt.space);
-		proc_len += putnchar('0', pcnt.zero);
-		write(1, str, pcnt.arg);
+		proc_len += putnchar(' ', note->space);
+		proc_len += putnchar('0', note->zero);
+		write(1, str, note->arg);
 	}
 	return (proc_len);
 }
