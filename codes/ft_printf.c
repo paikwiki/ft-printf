e3432f459073adfe56b0d73c40636ea606e397c8
cbaek <cbaek@student.42seoul.kr>
2020-08-27 20:52:07 +0900

    Refactor: be shorten if statement with ft_strchr()

--- codes/ft_printf.c
+++ codes/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: cbaek <cbaek@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/13 22:23:23 by cbaek             #+#    #+#             */
-/*   Updated: 2020/08/27 20:31:39 by cbaek            ###   ########.fr       */
+/*   Updated: 2020/08/27 20:49:34 by cbaek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,16 @@
 static void		setzero_note(t_note *note)
 {
 	note->flag = 0;
-	note->prcs = 0;
 	note->type = 0;
 	note->width = 0;
+	note->prcs = 0;
 	note->is_dot = 0;
 	note->cnt_space = 0;
 	note->cnt_zero = 0;
 	note->cnt_arg = 0;
 }
 
-static void		*parse_note(const char *fmt, int *idx, t_note *note,
-		va_list ap)
+static void		*parse_note(const char *fmt, int *idx, t_note *note, va_list ap)
 {
 	int	len;
 
@@ -56,10 +55,8 @@ static void		*parse_note(const char *fmt, int *idx, t_note *note,
 		if (fmt[*idx + len] == '*' && ++len)
 			note->prcs = va_arg(ap, int);
 		else
-		{
 			while (ft_isdigit(fmt[*idx + len]) && ++len)
 				note->prcs = (note->prcs * 10) + (fmt[*idx + len - 1] - '0');
-		}
 	}
 	note->type = fmt[*idx + len];
 	*idx = *idx + len;
@@ -93,7 +90,7 @@ static int		proc_ft_printf(const char *format, va_list ap)
 	int			idx;
 	size_t		proc_len;
 	size_t		total_len;
-	t_note	note;
+	t_note		note;
 
 	idx = 0;
 	total_len = 0;
@@ -106,7 +103,8 @@ static int		proc_ft_printf(const char *format, va_list ap)
 		{
 			setzero_note(&note);
 			parse_note(format, &idx, &note, ap);
-			if ((proc_len = proc_placeholder(note.type, ap, &note)) == 0 && (note.type != 's' && note.type != 'x' && note.type != 'X' && note.type != 'd' && note.type != 'i' && note.type != 'u'))
+			if ((proc_len = proc_placeholder(note.type, ap, &note)) == 0 &&
+					ft_strchr("diusxX", note.type) == 0)
 				return (ERROR);
 			total_len = total_len + proc_len;
 		}
